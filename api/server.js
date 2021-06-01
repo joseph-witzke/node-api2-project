77ac7007f4d436b73c8cacbc159aec88fa47d52e
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-01 15:16:36 -0600

    DELETE post working

--- api/server.js
+++ api/server.js
@@ -61,6 +61,49 @@ server.post('/api/posts', async (req, res) => {
     }
 })
 
+server.put('/api/posts/:id', async (req, res) => {
+    const { id } = req.params
+    const { body } = req
+    try {
+        if (!updatedPost) {
+            res.status(404).json({
+                message: `"The post with the specified ID ${id} does not exist"`
+            })
+        } else {
+            const updatedPost = await Posts.update(id, body)
+            res.status(200).json(updatedPost)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be modified",
+            error: err.message,
+        })
+    }
+})
+
+server.delete('/api/posts/:id', async (req, res) => {
+    const { id } = req.params
+    Posts.remove(id)
+        .then(deleted => {
+            if (!deleted) {
+                res.status(404).json({
+                    message: `"The post with the specified ID ${id} does not exist"`
+                })
+            } else {
+                res.json(deleted)
+            }
+        })
+        .catch (err => {
+            res.status(500).json({
+                message: "The post could not be removed",
+                error: err.message,
+            })
+        })
+})
+
+
+
+
 
 // EXPOSING THE SERVER TO OTHER MODULES
 module.exports = server

4a930458073a093a8c47bdfafad931a297b73be9
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-01 14:16:45 -0600

    Server up GET posts

--- api/server.js
+++ api/server.js
@@ -1,2 +1,30 @@
 // implement your server here
 // require your posts router and connect it here
+
+//imports
+
+const express = require('express')
+const Posts = require('./posts/posts-model')
+
+//Instances of express APP
+const server = express()
+
+//Global Middleware
+server.use(express.json())
+
+//Endpoints
+server.get('/api/posts', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res.json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The posts information could not be retrieved",
+        error: err.message,
+    })
+    })
+})
+
+
+// EXPOSING THE SERVER TO OTHER MODULES
+module.exports = server
